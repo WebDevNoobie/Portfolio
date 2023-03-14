3b0a20bd14a3fa3774dc7742747a86488ac97972
WebDevNoobie <81087655+WebDevNoobie@users.noreply.github.com>
2023-03-14 23:52:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import Skills from "./Components/Skills/Skills";
+import Work from "./Components/Work/Work";
+import "./styles.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+    </div>
+  );
+}
